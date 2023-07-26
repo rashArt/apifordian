e76616b6be0e6ccdb51b44c6edfaba9a70290138
rash07 <sh.raul.jgb@gmail.com>
2023-07-26 14:25:37 -0500

    ajustes para https

--- config/system_configuration.php
+++ config/system_configuration.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'allow_public_download' => env('ALLOW_PUBLIC_DOWNLOAD', true), 
+    'allow_public_download' => env('ALLOW_PUBLIC_DOWNLOAD', true),
+    'force_https' => env('FORCE_HTTPS', false),
 ];
