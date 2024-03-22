be705a7936413607f0cd896c1d2207156fac8dca
Alexander Obando <gerencia@torresoftware.com>
2024-03-22 15:38:22 -0500

    Parche request.php laravel

--- urn_on.bat
+++ urn_on.bat
@@ -1,2 +1,3 @@
 COPY resources\templates\xml\urn\*.* resources\templates\xml /Y
 COPY vendor\ubl21dian\torresoftware\src\XAdES\urn\*.* vendor\ubl21dian\torresoftware\src\XAdES /Y
+COPY resources\templates\xml\urn\Request.php vendor\laravel\framework\src\Illuminate\Http\Request.php
--- urn_on.sh
+++ urn_on.sh
@@ -1,2 +1,3 @@
 cp resources/templates/xml/urn/*.* resources/templates/xml
 cp vendor/ubl21dian/torresoftware/src/XAdES/urn/*.* vendor/ubl21dian/torresoftware/src/XAdES
+cp resources/templates/xml/urn/Request.php vendor/laravel/framework/src/Illuminate/Http/Request.php
