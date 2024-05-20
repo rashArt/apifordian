cd2bf87a1de6c410d8c0e916e08d24f0e905e64c
Alexander Obando <gerencia@torresoftware.com>
2024-05-20 17:50:08 -0500

    Updates spd 20-05-2024

--- database/migrations/2024_05_20_534745_update_payroll_type_document_identifications_table.php
+++ database/migrations/2024_05_20_534745_update_payroll_type_document_identifications_table.php
@@ -14,9 +14,9 @@ class UpdatePayrollTypeDocumentIdentificationsTable extends Migration
 	public function up()
 	{
         PayrollTypeDocumentIdentification::destroy(9);
-        PayrollTypeDocumentIdentification::create(['name' => 'PEP (Permiso Especial de Permanencia)', 'code' => '47']);
+        PayrollTypeDocumentIdentification::create(['id' => 9, 'name' => 'PEP (Permiso Especial de Permanencia)', 'code' => '47']);
         PayrollTypeDocumentIdentification::where('code', '47')->update(['id' => 9, 'name' => 'PEP (Permiso Especial de Permanencia)', 'code' => '47']);
-        PayrollTypeDocumentIdentification::create(['name' => 'PPT (Permiso Protección Temporal)', 'code' => '48']);
+        PayrollTypeDocumentIdentification::create(['id' => 12, 'name' => 'PPT (Permiso Protección Temporal)', 'code' => '48']);
         PayrollTypeDocumentIdentification::where('code', '48')->update(['id' => 12, 'name' => 'PPT (Permiso Protección Temporal)', 'code' => '48']);
     }
 
